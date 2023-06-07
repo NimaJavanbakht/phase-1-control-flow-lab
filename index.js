f463d4942568e3bc210cc21d50f7a841558fb34d
Nima Javanbakht <nimajavan230@gmail.com>
2023-06-06 18:10:57 -0700

    completes control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+
+  if (ride <= 400) {
+    ride = 'This one is on me!';
+    return ride;
+  }
+  else if (ride > 400 && ride <= 2000) {
+    ride = 'That will be twenty bucks.';
+    return ride;
+  }
+  else if (ride > 2000 && ride <= 2500) {
+    ride = 'I will gladly take your thirty bucks.';
+    return ride;
+  }
+  else 
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityMessage = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+
+  }
